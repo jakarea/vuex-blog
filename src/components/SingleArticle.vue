faed962e11ea89db2e4c83d074f22b69df0f2caf
Jakarea Parvez <jakareaparvez@gmail.com>
2017-11-27 04:11:39 +0600

    single post

--- src/components/SingleArticle.vue
+++ src/components/SingleArticle.vue
@@ -3,9 +3,16 @@
 		<div class="row">
 			<div class="col-md-12">
 				<div id="articles">
-					<h2 style="text-align:right"> Add New Post</h2>
-					<input v-model="message" placeholder="edit me">
-					<p>Message is: {{ message }}</p>
+					<h2 style="text:center"> Single Article</h2>
+					<div class="article">
+						<div class="title">{{ post.title }}</div>
+						<div class="body">{{ post.body }}</div>
+					</div>
+		
+					<div class="comments">Comments</div>
+					<div class="comment" v-for="comment in comments">
+						<div class="body">{{ comment.body }}</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -15,13 +22,35 @@
 <script>
 	
 	export default{
-		
+		created: function () {
+			var id = this.$route.params.postId
+		    this.$store.dispatch('post',id);
+		    this.$store.dispatch('comments',id);
+		 },
 		computed: {
-    		
+    		post(){
+    			return this.$store.getters.post;
+    		},
+    		comments(){
+    			return this.$store.getters.comments;
+    		}
 		}
 	}
 </script>
 
 <style>
-	
+	.comment {
+    padding: 10px 20px;
+    font-style: italic;
+}
+.title {
+    font-size: 20px;
+    font-weight: 700;
+    padding-bottom: 20px;
+}
+.comments{
+	font-size: 20px;
+    font-weight: 700;
+    padding-top: 20px;
+}
 </style>	
